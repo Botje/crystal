444b9c34580af885f74cc0f11b109391613d0919
Dries Harnie <dries.harnie@vub.ac.be>
2013-06-17 23:40:06 +0200

    Add --stats and --no-stats flag.

--- Crystal/Config.hs
+++ Crystal/Config.hs
@@ -11,11 +11,16 @@ data TSType = Dumb | Smart deriving (Show, Eq, Data, Typeable)
 data Config =
   Config { _cfgTypeSys   :: TSType,
            _cfgInputFile :: String,
-           _cfgCheckMobility :: Bool
+           _cfgCheckMobility :: Bool,
+           _cfgMobilityStats :: Bool
          } deriving (Show, Eq, Data, Typeable)
 
 $(makeLenses ''Config)
 
-defaultArgs = Config { _cfgTypeSys   = enum [ Smart &= help "Smart type system", Dumb &= help "Dynamic type system" ],
-                       _cfgInputFile = def &= args &= typFile,
-                       _cfgCheckMobility = True &= help "Test mobility" &= name "mobility" }
+defaultArgs = Config { _cfgTypeSys   = enum [ Smart &= help "Smart type system" &= name "s", Dumb &= help "Dynamic type system" &= name "d" ]
+                     , _cfgInputFile = def &= args &= typFile
+                     , _cfgCheckMobility = enum [ True &= help "Test mobility (default)" &= name "mobility" &= explicit,
+                                                  False &= help "Disable mobility" &= name "no-mobility" &= explicit ]
+                     , _cfgMobilityStats = enum [ True &= help "Generate mobility stats (default)" &= name "stats" &= explicit,
+                                                  False &= help "Disable stats generation" &= name "no-stats" &= explicit]
+                     }
