6333952c83b21b014406951b506c916ac3b3da39
Dries Harnie <dries.harnie@vub.ac.be>
2014-03-09 17:33:22 +0100

    First attempt at JS pretty printer

---
+++ misc/pretty.js
@@ -0,0 +1,158 @@
+$(function() {
+
+	String.prototype.repeat = String.prototype.repeat || function(num) {
+    return new Array(isNaN(num)? 1 : ++num).join(this);
+	}
+
+	var output;
+
+	function Empty() {
+	}
+
+	Empty.prototype.render = function(out) {
+	}
+
+	function Line() {
+	}
+
+	Line.prototype.render = function(out) {
+		out.append("\n");
+	}
+
+	function Text(t) {
+		this.text = t;
+	}
+	
+	Text.prototype.render = function(out) {
+		out.append(this.text);
+	}
+
+	function Cons(l, r) {
+		this.l = l;
+		this.r = r;
+	}
+	
+	Cons.prototype.render = function(out) {
+		this.l.render(out);
+		this.r.render(out);
+	}
+
+	function Nest(n, d) {
+		this.n = n;
+		this.d = d;
+	}
+
+	Nest.prototype.render = function(out) {
+		if (this.d instanceof Empty) {
+			this.d.render(out);
+		} else if (this.d instanceof Line) {
+			this.d.render(out);
+			out.append(" ".repeat(this.n));
+		} else if (this.d instanceof Text) {
+			this.d.render(out);
+		} else if (this.d instanceof Cons) {
+			new Cons(nest(this.n, this.d.l), nest(this.n, this.d.r)).render(out);
+		} else if (this.d instanceof Nest) {
+			new Nest(this.n + this.d.n, this.d.d).render(out);
+		}
+	}
+
+	function empty() {
+		return new Empty();
+	}
+
+	function text(t) {
+		return new Text(t);
+	}
+
+	function line() {
+		return new Line();
+	}
+
+	function cons() {
+		var params = Array.prototype.slice.call(arguments);
+		var doc = params.pop();
+		while (params.length)
+			doc = new Cons(params.pop(), doc);
+		return doc;
+	}
+
+	function consS() {
+		var params = Array.prototype.slice.call(arguments);
+		var doc = params.pop();
+		while (params.length)
+			doc = cons(params.pop(), text(" "), doc);
+		return doc;
+	}
+
+	function nest(n, d) {
+		return new Nest(n, d);
+	}
+
+	function indent(n, d) {
+		return nest(n, cons(line(), d));
+	}
+
+	function parens(d) {
+		return cons(text("("), d, text(")"));
+	}
+
+	function pretty(tree) {
+		return prettyPrint(tree);
+	}
+
+	function prettyPrint(tree) {
+		switch (tree.type) {
+			case "LetExpression": 
+				return parens(cons(text(tree.kw), text(" "),
+				       parens(cons(text(tree.head[0].id), text(" "), pretty(tree.head[0].init))),
+				       indent(2, pretty(tree.body))
+				       ));
+				break;
+
+			case "CallExpression":
+				var temp = tree.arguments.map(pretty);
+				temp.unshift(pretty(tree.callee));
+				return parens(consS.apply(null, temp));
+
+			case "FunctionExpression":
+				return parens(consS(text("lambda"),
+				                    parens(consS.apply(null, tree.params.map(text))),
+				                    indent(2, pretty(tree.body))))
+				return undefined;
+
+			case "SequenceExpression":
+				var exprs = tree.expressions.map(pretty);
+				exprs = exprs.map(function(x) { return cons(x, line()); });
+				return parens(cons(text("begin"), indent(2, cons.apply(null, exprs))));
+
+			case "ConditionalExpression":
+				return parens(consS(text("if"), pretty(tree.test), indent(2, pretty(tree.consequent)), indent(2, pretty(tree.alternative))));
+			case "Identifier":
+				return text(tree.name);
+
+			case "Literal":
+				if (tree.value == "True") 
+					return text("#t");
+				if (tree.value == "False") 
+					return text("#f");
+				if (tree.value == "#<void>") 
+					return text("#&lt;void>");
+				return text(tree.value);
+
+			default:
+				return text(tree.type);
+		}
+	}
+
+	$("#pretty-button").click(function(){
+		var input = $("#rawjson").val();
+		input = input.replace("<![CDATA[","");
+	  input = input.replace("]]>", "");
+		tree = JSON.parse(input);
+
+		output = $("#pretty-output").first();
+		output.empty();
+		pretty(tree).render(output);
+	});
+});
