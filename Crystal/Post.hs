0176bf9b03b32cc47c025e0c16fdd4d3ce033f37
Dries Harnie <dries.harnie@vub.ac.be>
2014-04-17 17:18:30 +0200

    Postprocessing: Fold ANF temporary variables into body

--- Crystal/Post.hs
+++ Crystal/Post.hs
@@ -25,10 +25,21 @@ undoLetrec expr = return (decls, core)
         go e = ([], e)
 
 undoLetLet :: Expr TLabel -> Step (Expr TLabel)
-undoLetLet = return . transform f
+undoLetLet = return . transformBi f
   where f e@(Expr l (Let [(id, app)] b)) =
           case b of
-               Expr _ (Let bnds bod) -> Expr l (Let ((id, app) : bnds) bod)
+               Expr l' (If cond cons alt)
+                  | "tmp-" `isPrefixOf` id -> Expr l (If (inline id app cond) cons alt)
+               Expr l' (Let bnds bod)
+                  | "tmp-" `isPrefixOf` id -> inline id app b
+                  | otherwise              -> Expr l (Let ((id, app) : bnds) bod)
+               Expr l' (Appl f args)
+                 | "tmp-" `isPrefixOf` id && not (isRefTo "check" f) -> inline id app b
                Expr _ (Ref id') | id' == id -> app
                _ -> e
+        f x = x
+
+inline :: Ident -> Expr TLabel -> Expr TLabel -> Expr TLabel
+inline id expr = transformBi f
+  where f (Expr _ (Ref var)) | var == id = expr
         f x = x
