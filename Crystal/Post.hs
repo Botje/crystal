06840977ed229c233ee925b89044e2efdca2d482
Dries Harnie <dries.harnie@vub.ac.be>
2015-03-13 17:41:38 +0100

    Post: remove some traversals
    12s -> 6s on dynamic.sch

--- Crystal/Post.hs
+++ Crystal/Post.hs
@@ -1,11 +1,14 @@
+{-# LANGUAGE PatternGuards #-}
 module Crystal.Post (postprocess, DeclExpr) where
 
 import Control.Lens hiding (transform, universe)
 import Control.Monad
 import Control.Monad.Reader
+import Control.Monad.Writer
 import Data.List
 import Data.Generics
 import Data.Generics.Biplate
+import qualified Data.Map as M
 
 import Crystal.AST
 import Crystal.Config
@@ -30,24 +33,39 @@ undoLetrec expr = do addDefines <- not `fmap` asks (^.cfgAnnotateLabels)
         go e = ([], e)
 
 undoLetLet :: Expr TLabel -> Step (Expr TLabel)
-undoLetLet = return . transformBi f
+undoLetLet expr = do let (expr', ps) = runWriter $ transformBiM f expr
+                     
+                     return $ inline (M.fromList ps) expr'
   where f e@(Expr l (Let [(id, app)] b)) =
           case b of
                Expr l' (If cond cons alt)
-                  | "tmp-" `isPrefixOf` id && 1 == id `mentionsIn` b -> Expr l (If (inline id app cond) cons alt)
+                  | isTmp && oneMention -> tell [(id, app)] >> return b
                Expr l' (Let bnds bod)
-                  | "tmp-" `isPrefixOf` id && 1 == id `mentionsIn` b -> inline id app b
-                  | otherwise              -> Expr l (Let ((id, app) : bnds) bod)
+                  | isTmp && oneMention -> tell [(id, app)] >> return b
+                  | otherwise              -> return $ Expr l (Let ((id, app) : bnds) bod)
                Expr l' (Appl f args)
-                 | "tmp-" `isPrefixOf` id && not (isRefTo "check" f) -> inline id app b
-               Expr _ (Ref id') | id' == id -> app
-               _ -> e
-        f x = x
+                  | isTmp && not (isRefTo "check" f) -> tell [(id,app)] >> return b
+               Expr _ (Ref id') | oneMention -> return app
+               _ -> return e
+            where isTmp = "tmp-" `isPrefixOf` id
+                  oneMention = Just 1 == (M.lookup id =<< M.lookup l mentions)
+        f x = return x
+        mentions = computeMentions expr
 
-mentionsIn :: Ident -> Expr TLabel -> Int
-mentionsIn id expr = length [ () | Expr _ (Ref id') <- universe expr, id == id' ]
+computeMentions :: Expr TLabel -> M.Map TLabel (M.Map Ident Int)
+computeMentions expr = mentionsMap
+  where mentionsMap = M.fromList [(l, mentions e)| Expr l e <- universeBi expr ]
+        mentions (Lit l)            = M.empty
+        mentions (Ref r)            = M.singleton r 1
+        mentions (Appl f args)      = M.unionsWith (+) (m f : map m args)
+        mentions (If cond cons alt) = M.unionsWith (+) [m cond, m cons, m alt]
+        mentions (Let bnds bod)     = M.unionsWith (+) (m bod : map (m . snd) bnds)
+        mentions (LetRec bnds bod)  = M.unionsWith (+) (m bod : map (m . snd) bnds)
+        mentions (Lambda ids r bod) = m bod
+        mentions (Begin es)         = M.unionsWith (+) (map m es)
+        m e = mentionsMap M.! (e ^. ann)
 
-inline :: Ident -> Expr TLabel -> Expr TLabel -> Expr TLabel
-inline id expr = transformBi f
-  where f (Expr _ (Ref var)) | var == id = expr
+inline :: M.Map Ident (Expr TLabel) -> Expr TLabel -> Expr TLabel
+inline m expr = transformBi f expr
+  where f (Expr _ (Ref var)) | Just expr' <- M.lookup var m = expr'
         f x = x
